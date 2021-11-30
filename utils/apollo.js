32e03493b169d2dced3dcffc03043a5de4e92e8e
tedspare <ted.spare@gmail.com>
2021-11-29 20:51:08 -0500

    Add Apollo middleware to get API token

--- utils/apollo.js
+++ utils/apollo.js
@@ -1,8 +1,40 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  HttpLink,
+  ApolloLink,
+  InMemoryCache,
+  concat,
+} from "@apollo/client";
+import supabase from "./supabase";
 
-export const apollo = (token) =>
-  new ApolloClient({
-    uri: "https://api.github.com/graphql",
-    cache: new InMemoryCache(),
-    headers: { authorization: `Bearer ${token}` },
-  });
+// Not sure what this does. See https://tinyurl.com/y93u9k5k.
+const httpLink = new HttpLink({ uri: "https://api.github.com/graphql" });
+
+/**
+ * Before every request, get the user's GitHub API token
+ */
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // Get the API token from the Supabase session
+  const session = supabase.auth.session();
+  const token = session.provider_token;
+
+  // Add the token to the request context
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`,
+    },
+  }));
+
+  return forward(operation);
+});
+
+/**
+ * Apollo client to make calls to GitHub graphQL
+ */
+const apollo = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
+});
+
+export default apollo;
