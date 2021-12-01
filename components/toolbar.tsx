9e68317a01334b0df8fdd8c8042cb867570d2fc0
Sarim Malik <sarimrasheed@gmail.com>
2021-11-30 19:52:50 -0500

    Make download more responsive

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -1,10 +1,23 @@
-import { TwitterLogoIcon, DownloadIcon, CopyIcon } from "@modulz/radix-icons";
+import { useState } from "react";
+import {
+  TwitterLogoIcon,
+  DownloadIcon,
+  CopyIcon,
+  CheckIcon,
+} from "@modulz/radix-icons";
 import Tooltip from "../components/tooltip";
 import domtoimage from "dom-to-image";
 import saveAs from "file-saver";
 
 function Toolbar() {
-  const buttonClass = "p-5 hover:bg-gray-500 hover:bg-opacity-20 rounded";
+  const [downloaded, setDownloaded] = useState(false);
+
+  setTimeout(() => {
+    setDownloaded(false);
+  }, 3 * 1000);
+
+  const buttonClass =
+    "p-2 hover:bg-gray-500 hover:bg-opacity-20 rounded scale-[1.5] hover:scale-[1.8]";
 
   function download() {
     let canvas = document.getElementById("wrap");
@@ -13,7 +26,7 @@ function Toolbar() {
     });
   }
   return (
-    <div className="mt-5 text-white space-x-3">
+    <div className="p-7 text-white space-x-7">
       <Tooltip content="Copy image">
         <button className={buttonClass}>
           <CopyIcon />
@@ -27,8 +40,14 @@ function Toolbar() {
       </Tooltip>
 
       <Tooltip content="Download">
-        <button className={buttonClass} onClick={download}>
-          <DownloadIcon />
+        <button
+          className={buttonClass}
+          onClick={() => {
+            download();
+            setDownloaded(true);
+          }}
+        >
+          {downloaded ? <CheckIcon /> : <DownloadIcon />}
         </button>
       </Tooltip>
     </div>
