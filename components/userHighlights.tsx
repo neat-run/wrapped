da323f57535d6a7decca00a375086af87828c888
tedspare <ted.spare@gmail.com>
2021-12-02 15:40:28 -0500

    Handle other-user case in user highlights

--- components/userHighlights.tsx
+++ components/userHighlights.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 import Constants from "../utils/constants";
+import { User } from "../types/common";
 
 // GraphQL query to get an overview of a user's contributions
 const USER_HIGHLIGHTS = gql`
@@ -20,55 +21,63 @@ const USER_HIGHLIGHTS = gql`
   }
 `;
 
+interface Props {
+  hostUser?: User;
+}
+
 /**
  * Fetch and display the user's top
  * @returns {element} div with text
  */
-function UserHighlights() {
+function UserHighlights({ hostUser }: Props) {
+  // Fetch the current user's stats
   const { data } = useQuery(USER_HIGHLIGHTS, {
-    variables: { start: Constants.DATES.JAN2021, end: Constants.DATES.DEC2021 },
+    variables: {
+      start: Constants.DATES.JAN2021,
+      end: Constants.DATES.DEC2021,
+    },
   });
 
-  if (!data || !data.viewer) return <></>;
-
-  const contributions = data.viewer.contributionsCollection;
+  // Serialize stats
+  const stats = useMemo(() => {
+    if (hostUser) return hostUser;
+    else if (data && data.viewer) {
+      const collection = data.viewer.contributionsCollection;
+      return {
+        commits: collection.totalCommitContributions,
+        contributions: collection.contributionCalendar.totalContributions,
+        pulls: collection.totalPullRequestContributions,
+        repos: collection.totalRepositoriesWithContributedCommits,
+        reviews: collection.totalPullRequestReviewContributions,
+      };
+    }
+  }, []);
 
   return (
     <div className="p-5 text-left space-y-5 text-white">
       <div>
         You have no commitment issues
-        <p className="text-5xl font-bold">
-          {contributions.totalCommitContributions}
-        </p>
+        <p className="text-5xl font-bold">{stats.commits}</p>
         commits
       </div>
       <div>
         You carry your weight
-        <p className="text-5xl font-bold">
-          {contributions.contributionCalendar.totalContributions}
-        </p>
+        <p className="text-5xl font-bold">{stats.contributions}</p>
         total contributions
       </div>
       <div>
         You code far and wide
-        <p className="text-5xl font-bold">
-          {contributions.totalRepositoriesWithContributedCommits}
-        </p>
+        <p className="text-5xl font-bold">{stats.repos}</p>
         repos
       </div>
       <div>
         PRs are your forte
-        <p className="text-5xl font-bold">
-          {contributions.totalPullRequestContributions +
-            contributions.totalPullRequestReviewContributions}
-        </p>
+        <p className="text-5xl font-bold">{stats.pulls}</p>
         contributions
       </div>
       <div>
         Your team loves you for your
-        <p className="text-5xl font-bold">
-          {contributions.totalPullRequestReviewContributions}
-        </p>
+        <p className="text-5xl font-bold">{stats.reviews}</p>
         PR reviews
       </div>
     </div>
