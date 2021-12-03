b6cfadd46475b653b723a554111b9ed9c52d4d2b
Sarim Malik <sarimrasheed@gmail.com>
2021-12-03 01:33:33 -0500

    Make top repo query accurate, add bar chart

---
+++ components/barChart.tsx
@@ -0,0 +1,25 @@
+import { Bar } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+
+// Bar chart component
+export const BarChart = ({ chartData, title }) => {
+  Chart.register(...registerables);
+  return (
+    <div>
+      <Bar
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: title,
+            },
+            legend: {
+              display: false,
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
--- components/topRepos.tsx
+++ components/topRepos.tsx
@@ -1,27 +1,72 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { BarChart } from "./barChart";
 
 // GraphQL query to get an overview of a user's contributions
 const TOP_REPOS = gql`
-  query {
+  query topRepos {
     viewer {
-      repositoriesContributedTo(
-        first: 5
-        orderBy: { field: STARGAZERS, direction: DESC }
-        contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]
-      ) {
-        totalCount
-        nodes {
-          nameWithOwner
-          url
-          commitComments {
+      name
+      login
+      contributionsCollection {
+        issueContributionsByRepository(maxRepositories: 5) {
+          contributions {
             totalCount
           }
-          owner {
-            avatarUrl
+          repository {
+            name
+            nameWithOwner
+            url
+            isPrivate
+            owner {
+              avatarUrl
+            }
+            stargazerCount
           }
-          stargazers {
+        }
+        pullRequestContributionsByRepository(maxRepositories: 5) {
+          contributions {
             totalCount
+          }
+          repository {
+            name
+            nameWithOwner
+            url
+            isPrivate
+            owner {
+              avatarUrl
+            }
+            stargazerCount
+          }
+        }
+        pullRequestReviewContributionsByRepository(maxRepositories: 5) {
+          contributions {
+            totalCount
+          }
+          repository {
+            name
+            nameWithOwner
+            url
+            isPrivate
+            owner {
+              avatarUrl
+            }
+            stargazerCount
+          }
+        }
+        commitContributionsByRepository(maxRepositories: 5) {
+          contributions {
+            totalCount
+          }
+          repository {
+            name
+            nameWithOwner
+            url
+            isPrivate
+            owner {
+              avatarUrl
+            }
+            stargazerCount
           }
         }
       }
@@ -34,23 +79,52 @@ function TopRepos() {
 
   if (!data || !data.viewer) return <></>;
 
+  const repos =
+    data.viewer.contributionsCollection.commitContributionsByRepository;
+
+  // Formatting data in chart-friendly format
+  const chartData = {
+    labels: repos.map((repo) => repo.repository.name),
+    datasets: [
+      {
+        data: repos.map((repo) => repo.contributions.totalCount),
+        backgroundColor: [
+          "#ffbb11",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderWidth: 0,
+      },
+    ],
+  };
+
   return (
     <div className="p-5 text-left space-y-5 text-white">
       <p>You're an absolute beast</p>
-      {data.viewer.repositoriesContributedTo.nodes.map((repo, i) => (
+      <BarChart chartData={chartData} title="Top Repositories" />
+      {repos.map((repo, i) => (
         <div key={i} className="flex items-center space-x-2">
           <img
-            className="w-20 h-20 rounded-full"
-            src={repo.owner.avatarUrl}
-            alt={repo.nameWithOwner + " logo"}
+            className="w-10 h-10 rounded-full"
+            src={repo.repository.owner.avatarUrl}
+            alt={repo.repository.name + " logo"}
           />
           <div className="flex items-end space-x-2">
             <div>
-              <a href={repo.url} rel="noopener noreferrer">
-                {repo.nameWithOwner}
-              </a>
-              <p className="text-3xl font-bold">{repo.stargazers.totalCount}</p>
-              stars
+              {repo.repository.isPrivate ? (
+                <p>{repo.repository.nameWithOwner}</p>
+              ) : (
+                <a href={repo.repository.url} rel="noopener noreferrer">
+                  {repo.repository.nameWithOwner}
+                </a>
+              )}
+              <p className="text-xs">⭐ {repo.repository.stargazerCount}</p>
+              <p className="text-3xl font-bold">
+                {repo.contributions.totalCount}
+              </p>{" "}
+              commits
             </div>
           </div>
         </div>
