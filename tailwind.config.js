f369ca984866c80b24dc342947789dc44fafd6d4
tedspare <ted.spare@gmail.com>
2021-12-03 13:02:06 -0500

    Add flat gray to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
+      green: colors.green,
+      yellow: colors.amber,
+      purple: colors.purple,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      red: colors.red,
+    },
     extend: {
       gridTemplateRows: {
         7: "repeat(7, minmax(0, 1fr))",
