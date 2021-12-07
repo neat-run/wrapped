5ebb0db9496ef3459c20c82b9e0e04651ed8c054
tedspare <ted.spare@gmail.com>
2021-12-06 21:36:41 -0500

    Add full name to loading page

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,21 +1,32 @@
+import { User } from "../types/common";
+
+interface IProps {
+  user: User;
+}
+
 /**
  * Loading component
  * @param {User} user for username etc
  */
-function Loading({ user }) {
+function Loading({ user }: IProps) {
   return (
-    <div className="flex flex-col items-center text-gray-100 p-5 space-y-4">
-      {user.avatarUrl && (
-        <img
-          className="w-12 h-12 rounded-full"
-          src={user.avatarUrl}
-          alt={`${user.username}'s avatar'`}
-        />
-      )}
-      <p className="text-2xl font-medium text-white">
-        Welcome, {user.username}.
+    <div className="text-center text-gray-100 p-5 space-y-10">
+      <p className="text-3xl text-white space-x-2">
+        {user.avatarUrl && (
+          <img
+            className="w-20 h-20 rounded-full mx-auto mb-4"
+            src={user.avatarUrl}
+            alt={`${user.username}'s avatar'`}
+          />
+        )}
+        <span>Welcome,</span>
+        <span className="font-mono tracking-tighter text-transparent bg-clip-text bg-gradient-to-l to-[#85259D] via-indigo-600 from-[#6B3EEC]">
+          {user.fullName ? user.fullName : user.username}
+        </span>
       </p>
-      <div className="text-3xl text-white animate-pulse">Loading...</div>
+      <div className="text-xl font-medium text-gray-400 animate-pulse">
+        Loading...
+      </div>
       {/* Add progress bar for better UX */}
     </div>
   );
