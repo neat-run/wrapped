acb0990038d253ab219f594a8565e579aa44913a
tedspare <ted.spare@gmail.com>
2021-12-06 19:00:53 -0500

    Move username to loading component

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,8 +1,21 @@
-import ProgressBar from "./progressBar";
-function Loading() {
+/**
+ * Loading component
+ * @param {User} user for username etc
+ */
+function Loading({ user }) {
   return (
-    <div className="text-3xl text-white animate-pulse">
-      <div>Loading...</div>
+    <div className="flex flex-col items-center text-gray-100 p-5 space-y-4">
+      {user.avatarUrl && (
+        <img
+          className="w-12 h-12 rounded-full"
+          src={user.avatarUrl}
+          alt={`${user.username}'s avatar'`}
+        />
+      )}
+      <p className="text-2xl font-medium text-white">
+        Welcome, {user.username}.
+      </p>
+      <div className="text-3xl text-white animate-pulse">Loading...</div>
       {/* Add progress bar for better UX */}
     </div>
   );
