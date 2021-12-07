a321e8099141f31296fd7eb2d3bff5ad3a5fde7e
tedspare <ted.spare@gmail.com>
2021-12-06 20:38:35 -0500

    Format date in contributions

--- components/contributions.tsx
+++ components/contributions.tsx
@@ -34,22 +34,30 @@ function Contributions({ user }: IProps) {
     let rounded = Math.ceil(normalized / 0.25) * 0.25;
 
     // Assign color for each case
-    if (rounded === 0) colors[value] = "bg-gray-200 bg-opacity-20";
-    if (rounded === 0.25) colors[value] = "bg-green-200 bg-opacity-80";
-    if (rounded === 0.5) colors[value] = "bg-green-400 bg-opacity-80";
-    if (rounded === 0.75) colors[value] = "bg-green-600 bg-opacity-80";
-    if (rounded === 1) colors[value] = "bg-green-800 bg-opacity-80";
+    if (rounded === 0) colors[value] = "bg-gray-200/20";
+    if (rounded === 0.25) colors[value] = "bg-green-200/80";
+    if (rounded === 0.5) colors[value] = "bg-green-400/80";
+    if (rounded === 0.75) colors[value] = "bg-green-600/80";
+    if (rounded === 1) colors[value] = "bg-green-800/80";
   });
 
   return (
-    <div>
-      <div className="text-white text-left p-2">You show up daily</div>
+    <div className="space-y-5 p-5 max-w-4xl mx-auto">
+      <div className="text-gray-400 text-xl font-medium text-left px-2">
+        You show up daily
+      </div>
       <div className="p-2 pt-0 grid gap-1 text-white grid-rows-7 grid-flow-col">
         {weeks.map((week, i) =>
           week.contributionDays.map((day, j) => (
             <Tooltip
               key={j}
-              content={`${day.contributionCount} contributions on ${day.date}`}
+              content={`${day.contributionCount} contributions on ${new Date(
+                day.date
+              )
+                .toDateString()
+                .split(" ")
+                .slice(1, 4)
+                .join(" ")}`}
             >
               <div
                 key={j}
