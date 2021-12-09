4df8aa943e82071b1071bd3fa1aeb5827a7fbb28
tedspare <ted.spare@gmail.com>
2021-12-09 14:53:48 -0500

    Upgrade Tailwind to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+    "./index.html",
+  ],
   theme: {
     colors: {
       transparent: "transparent",
-      gray: colors.gray,
+      gray: colors.trueGray,
       white: colors.white,
       black: colors.black,
       green: colors.green,
