1bd342558b4badbd314e87f5c52a3dff128b3cf1
loreina <loreina.chew@gmail.com>
2021-12-13 16:42:34 +0800

    style: made by neat at top of footer on smaller screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,26 +95,31 @@ export default function Home({ hostUser }) {
           <SignIn auth={auth} setAuth={setAuth} />
         )}
       </main>
-      <footer>
-        <div className="px-8 w-screen flex justify-between">
-          <a
-            href="https://github.com/neat-run/wrapped"
-            className="text-gray-500 hover:text-gray-200 p-3 transition-transform hover:-translate-y-1 hover:rotate-3 duration-500"
-            rel="noopener noreferrer"
-          >
-            Code
-          </a>
-          <div className="flex items-baseline">
-            <span className="text-gray-500">Made by</span>
+      <footer className="px-0 md:px-8 w-1/5 md:w-screen flex flex-wrap justify-center md:justify-between">
+        <div className="flex items-center order-first md:order-2">
+          <div className="flex flex-col sm:flex-row text-gray-400 text-center justify-center">
+            Made by &nbsp;
             <a
-              className="font-semibold p-3 pl-1 text-indigo-500 hover:text-indigo-400 transition hover:-translate-y-1 duration-500"
+              className="font-semibold text-indigo-500 hover:text-indigo-400 transition hover:-translate-y-1 duration-500"
               href={Constants.NEAT.URL}
             >
               Neat
             </a>
           </div>
+        </div>
+        <div className="flex order-1">
+          <a
+            href="https://github.com/neat-run/wrapped"
+            className="text-gray-400 hover:text-gray-200 p-3 transition-transform hover:-translate-y-1 hover:rotate-3 duration-500"
+            rel="noopener noreferrer"
+          >
+            Code
+          </a>
+        </div>
+
+        <div className="flex order-3">
           <Link href="/privacy">
-            <a className="text-gray-500 hover:text-gray-200 p-3 transition-transform hover:-translate-y-1 hover:-rotate-3 duration-500">
+            <a className="text-gray-400 hover:text-gray-200 p-3 transition-transform hover:-translate-y-1 hover:-rotate-3 duration-500">
               Privacy
             </a>
           </Link>
